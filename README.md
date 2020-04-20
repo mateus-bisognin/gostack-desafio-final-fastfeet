770a0a00db993faf8bc26d74d4d71e4db3f1ae0a
mateus-bisognin <mateus_bisognin@hotmail.com>
2020-04-20 00:00:27 -0300

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gostack-desafio-final-fastfeet
+
 Desafio final do Bootcamp GoStack da Rocketseat para certificação.
+
+Para execução, o backend precisa se conectar ao PostGres e ao Redis.
---
+++ fastfeetmobile/src/components/Delivery/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View``;
+export const Title = styled.View``;
+export const Status = styled.View``;
+export const Data = styled.View``;
